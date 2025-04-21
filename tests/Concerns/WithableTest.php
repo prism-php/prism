b7d75e67383eba82789b515b1d26e4f333a4a2e3
Konstantin Auffinger <62616071+kauffinger@users.noreply.github.com>
2025-04-21 09:55:28 +0200

    chore: add more tests for the withable trait to make functionality more clear

--- tests/Concerns/WithableTest.php
+++ tests/Concerns/WithableTest.php
@@ -16,3 +16,52 @@ it('can update readonly properties by copying the class', function (): void {
         ->and($newInstance::class)->toBe($instance::class)
         ->and($instance->foo)->toBe('bar');
 });
+
+it('throws if the property does not exist', function (): void {
+    $instance = new class
+    {
+        use Withable;
+
+        public function __construct(public readonly string $foo = 'bar') {}
+    };
+
+    $instance->withBaz('baz');
+})->throws(\BadMethodCallException::class, 'Method withBaz does not exist.');
+
+it('can still call other methods', function (): void {
+    $instance = new class
+    {
+        use Withable;
+
+        public function __construct(public readonly string $foo = 'bar') {}
+
+        public function test(string $foo): self
+        {
+            return new self($foo);
+        }
+    };
+
+    $newInstance = $instance->test('baz');
+
+    expect($newInstance->foo)->toBe('baz')
+        ->and($instance->foo)->toBe('bar');
+});
+
+it('will prefer existing methods over properties', function (): void {
+    $instance = new class
+    {
+        use Withable;
+
+        public function __construct(public readonly string $foo = 'bar') {}
+
+        public function withFoo(string $foo): self
+        {
+            return new self('not baz');
+        }
+    };
+
+    $newInstance = $instance->withFoo('baz');
+
+    expect($newInstance->foo)->toBe('not baz')
+        ->and($instance->foo)->toBe('bar');
+});
