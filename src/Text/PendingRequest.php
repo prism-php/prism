e0576ba3a084e390376bb31d3c798f636e1e8d76
Chris Bridges <chris.bridges@tacit.legal>
2025-06-23 16:59:59 +0100

    fix: make PendingRequest only catch RequestExceptions

--- src/Text/PendingRequest.php
+++ src/Text/PendingRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Prism\Prism\Text;
 
 use Generator;
+use Illuminate\Http\Client\RequestException;
 use Prism\Prism\Concerns\ConfiguresClient;
 use Prism\Prism\Concerns\ConfiguresGeneration;
 use Prism\Prism\Concerns\ConfiguresModels;
@@ -17,7 +18,6 @@ use Prism\Prism\Concerns\HasProviderTools;
 use Prism\Prism\Concerns\HasTools;
 use Prism\Prism\Exceptions\PrismException;
 use Prism\Prism\ValueObjects\Messages\UserMessage;
-use Throwable;
 
 class PendingRequest
 {
@@ -46,7 +46,7 @@ class PendingRequest
 
         try {
             return $this->provider->text($request);
-        } catch (Throwable $e) {
+        } catch (RequestException $e) {
             $this->provider->handleRequestExceptions($request->model(), $e);
         }
     }
@@ -64,7 +64,7 @@ class PendingRequest
             foreach ($chunks as $chunk) {
                 yield $chunk;
             }
-        } catch (Throwable $e) {
+        } catch (RequestException $e) {
             $this->provider->handleRequestExceptions($request->model(), $e);
         }
     }
