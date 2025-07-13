6b036c0a0b6819100e1a1c7936b8392ee9ec9daf
TJ Miller <tj@themillers.co>
2025-07-13 14:44:25 -0400

    wip

--- src/Concerns/HasPrompts.php
+++ src/Concerns/HasPrompts.php
@@ -5,6 +5,13 @@ declare(strict_types=1);
 namespace Prism\Prism\Concerns;
 
 use Illuminate\Contracts\View\View;
+use Prism\Prism\ValueObjects\Messages\Support\Audio;
+use Prism\Prism\ValueObjects\Messages\Support\Document;
+use Prism\Prism\ValueObjects\Messages\Support\Image;
+use Prism\Prism\ValueObjects\Messages\Support\Media;
+use Prism\Prism\ValueObjects\Messages\Support\OpenAIFile;
+use Prism\Prism\ValueObjects\Messages\Support\Text;
+use Prism\Prism\ValueObjects\Messages\Support\Video;
 use Prism\Prism\ValueObjects\Messages\SystemMessage;
 
 trait HasPrompts
@@ -12,13 +19,22 @@ trait HasPrompts
     protected ?string $prompt = null;
 
     /**
+     * @var array<int, Audio|Text|Image|Media|Document|OpenAIFile|Video>
+     */
+    protected array $additionalContent = [];
+
+    /**
      * @var SystemMessage[]
      */
     protected array $systemPrompts = [];
 
-    public function withPrompt(string|View $prompt): self
+    /**
+     * @param  array<int, Audio|Text|Image|Media|Document|OpenAIFile|Video>  $additionalContent
+     */
+    public function withPrompt(string|View $prompt, array $additionalContent = []): self
     {
         $this->prompt = is_string($prompt) ? $prompt : $prompt->render();
+        $this->additionalContent = $additionalContent;
 
         return $this;
     }
