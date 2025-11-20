67b293281fa7cfc64f2ba6a5a3b39e866ce8abf0
João Salomão <joaosalomao.mcp@gmail.com>
2025-11-20 11:15:46 -0300

    refactor(perplexity): use HandleHttpRequests concern

--- src/Providers/Perplexity/Handlers/Structured.php
+++ src/Providers/Perplexity/Handlers/Structured.php
@@ -8,15 +8,17 @@ use Prism\Prism\Enums\FinishReason;
 use Prism\Prism\Providers\Perplexity\Concerns\ExtractsAdditionalContent;
 use Prism\Prism\Providers\Perplexity\Concerns\ExtractsMeta;
 use Prism\Prism\Providers\Perplexity\Concerns\ExtractsUsage;
+use Prism\Prism\Providers\Perplexity\Concerns\HandlesHttpRequests;
 use Prism\Prism\Structured\Request as StructuredRequest;
 use Prism\Prism\Structured\Response as StructuredResponse;
 use Prism\Prism\ValueObjects\Messages\SystemMessage;
 
-class Structured extends BaseHandler
+class Structured
 {
     use ExtractsAdditionalContent;
     use ExtractsMeta;
     use ExtractsUsage;
+    use HandlesHttpRequests;
 
     public function __construct(
         protected PendingRequest $client,
