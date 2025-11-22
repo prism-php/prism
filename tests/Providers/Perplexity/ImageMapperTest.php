519b8f3ede35a9898d1671db68f136ef3a79bdca
João Salomão <joaosalomao.mcp@gmail.com>
2025-11-22 16:23:16 -0300

    feat(perplexity): validating allowed image mime types

--- tests/Providers/Perplexity/ImageMapperTest.php
+++ tests/Providers/Perplexity/ImageMapperTest.php
@@ -37,6 +37,20 @@ it('maps image base64 to payload', function (): void {
     ]);
 });
 
+it('map files with the allowed mime types', function (string $mimeType): void {
+    $base64Data = base64_encode('fake-image-content');
+    $media = new Image(base64: $base64Data, mimeType: $mimeType);
+    $mapper = new ImageMapper(media: $media);
+
+    expect($mapper->toPayload())->toBeArray();
+})->with(ImageMapper::SUPPORTED_MIME_TYPES);
+
+it('doesnt map image with unsupported mime type', function (): void {
+    $base64Data = base64_encode('fake-image-content');
+    $media = new Image(base64: $base64Data, mimeType: 'image/bmp');
+    new ImageMapper(media: $media);
+})->throws(PrismException::class);
+
 it("doesn't map image with invalid media", function (): void {
     $media = new Image;
     new ImageMapper(media: $media);
