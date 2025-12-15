58d7af1c09690e3515ff4a98a6519603e86550c7
Julian Hundeloh <5358638+jaulz@users.noreply.github.com>
2025-12-15 10:19:55 +0100

    fix: pass thinking only if available

--- src/Providers/Anthropic/Handlers/Stream.php
+++ src/Providers/Anthropic/Handlers/Stream.php
@@ -487,10 +487,10 @@ class Stream
         if ($toolResults !== []) {
             $request->addMessage(new AssistantMessage(
                 content: $this->state->currentText(),
-                additionalContent: [
+                additionalContent: !empty($this->state->currentThinking()) ? [
                     'thinking' => $this->state->currentThinking(),
                     'thinking_signature' => $this->state->currentThinkingSignature(),
-                ],
+                ] : [],
                 toolCalls: $toolCalls
             ));
 
