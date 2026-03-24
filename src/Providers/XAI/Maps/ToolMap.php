25616698134bf0c65b81a80f0aaeab2dbe9ca843
Cbrad24 <Cbrad24@hotmail.com>
2026-03-24 21:52:09 +1000

    Cast empty tool properties to object in xAI ToolMap

--- src/Providers/XAI/Maps/ToolMap.php
+++ src/Providers/XAI/Maps/ToolMap.php
@@ -21,7 +21,7 @@ class ToolMap
                 'description' => $tool->description(),
                 'parameters' => [
                     'type' => 'object',
-                    'properties' => $tool->parametersAsArray(),
+                    'properties' => (object) $tool->parametersAsArray(),
                     'required' => $tool->requiredParameters(),
                 ],
             ],
